f9f59777f9e04a512dd73c9cc6b889f817bb6e6a
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-03-22 17:31:49 +0100

    Add files via upload

---
+++ adb.tf
@@ -0,0 +1,15 @@
+## Copyright © 2021, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+module "oci-adb" {
+  source                    = "github.com/oracle-quickstart/oci-adb"
+  adb_database_db_name      = "myadbdb"
+  adb_database_display_name = "myadbdb"
+  adb_password              = var.adb_password
+  adb_database_db_workload  = "AJD" # Autonomous JSON Database (AJD)
+  compartment_ocid          = var.compartment_ocid
+  use_existing_vcn          = false # no network to inject
+  adb_private_endpoint      = false # no private endpoint (AJD exposed to public Internet)
+  whitelisted_ips           = [""]
+}
+
