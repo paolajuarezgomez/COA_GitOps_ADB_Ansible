8adaf41c4874b27d1cd68ba0afc9fe11a0ae1867
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-03-22 17:52:19 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -20,7 +20,7 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = "user.pem"
+  private_key_path = var.api_private_key_path
   region           = var.region
 }
 
