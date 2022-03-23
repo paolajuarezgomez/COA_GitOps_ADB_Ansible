6ab4178dd3a5ae705cd9aed30d63d9c7e929a6aa
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-03-23 10:41:03 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,14 +2,14 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 
-terraform {
-  required_providers {
-    oci = {
-      source = "oracle/oci"
-      version = "4.68.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    oci = {
+#      source = "oracle/oci"
+#      version = "4.68.0"
+#    }
+#  }
+#}
 
 
 provider "oci" {
