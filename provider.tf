cf887baa3a505b66dc160ebcdb29c849cbb0d81b
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-03-23 10:22:45 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,16 @@
 ## Copyright © 2021, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
+
 terraform {
-  required_version = ">= 0.14"
   required_providers {
     oci = {
-      source  = "hashicorp/oci"
-      
-    }
-    
-    random = {
-      source  = "hashicorp/random"
-      version = "2.3.0" 
+      source = "oracle/oci"
+      version = "4.68.0"
     }
   }
 }
+
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
