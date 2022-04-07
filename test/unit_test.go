be5f9eff109b16dc20fc68a7d22017ef774b0788
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-04-07 13:48:10 +0200

    Update and rename static_site_unit_test.go to unit_test.go

---
+++ test/unit_test.go
@@ -0,0 +1,18 @@
+// +build unit
+
+package test
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStaticSiteValidity(t *testing.T) {
+	t.Parallel()
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../terraform",
+	})
+	output := terraform.InitAndPlan(t, terraformOptions)
+	assert.Contains(t, output, "3 to add, 0 to change, 0 to destroy")
+}
