43581fb13c3a07b5e3884dae0e492d1d303c182c
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-04-11 14:01:29 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -13,7 +13,7 @@ variable "tenancy_id" {
 }
 
 # Get this from OCI > Identity > Users (for your user account)
-variable "user_id" {
+variable "user_ocid" {
   description = "Get this from OCI > Identity > Users (for your user account)"
 }
 
