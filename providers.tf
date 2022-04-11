f24b006070a1c155c04f27362cc131b232209097
paolajuarezgomez <paola.juarez.gomez@gmail.com>
2022-04-11 14:04:57 +0200

    files

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_id
-  user_ocid        = var.user_id
+  user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
   region           = var.region
