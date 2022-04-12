8a0402146a0daa3bb4759bd625c8f24b2cc6a19c
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-04-12 18:04:41 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -10,16 +10,19 @@
 # Get this from the bottom of the OCI screen (after logging in, after Tenancy ID: heading)
 variable "tenancy_id" {
   description = "Get this from the bottom of the OCI screen (after logging in, after Tenancy ID: heading)"
+  sensitive   = true
 }
 
 # Get this from OCI > Identity > Users (for your user account)
 variable "user_id" {
   description = "Get this from OCI > Identity > Users (for your user account)"
+  sensitive   = true
 }
 
 # the fingerprint can be gathered from your user account (OCI > Identity > Users > click your username > API Keys fingerprint (select it, copy it and paste it below))
 variable "fingerprint" {
   description = "The fingerprint can be gathered from your user account (OCI > Identity > Users > click your username > API Keys fingerprint (select it, copy it and paste it below))"
+  sensitive   = true
 }
 
 # this is the full path on your local system to the private key used for the API key pair
@@ -205,6 +208,7 @@ variable "deploy_adb" {
 variable "adb_password" {
   type        = string
   description = "Initial ADB ADMIN user password"
+  sensitive   = true
 }
 
 variable "adb_db_name" {
