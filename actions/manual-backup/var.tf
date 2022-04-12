76f932a577e2a6a16676f050b266729abcaea851
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-04-12 18:05:18 +0200

    Update var.tf

--- actions/manual-backup/var.tf
+++ actions/manual-backup/var.tf
@@ -6,16 +6,19 @@
 # Get this from the bottom of the OCI screen (after logging in, after Tenancy ID: heading)
 variable "tenancy_id" {
   description = "Get this from the bottom of the OCI screen (after logging in, after Tenancy ID: heading)"
+  sensitive   = true
 }
 
 # Get this from OCI > Identity > Users (for your user account)
 variable "user_id" {
   description = "Get this from OCI > Identity > Users (for your user account)"
+  sensitive   = true
 }
 
 # the fingerprint can be gathered from your user account (OCI > Identity > Users > click your username > API Keys fingerprint (select it, copy it and paste it below))
 variable "fingerprint" {
   description = "The fingerprint can be gathered from your user account (OCI > Identity > Users > click your username > API Keys fingerprint (select it, copy it and paste it below))"
+  sensitive   = true
 }
 
 # this is the full path on your local system to the private key used for the API key pair
@@ -40,9 +43,11 @@ variable "adb_id" {
 
 variable "AWS_ACCESS_KEY_ID" {
   description = ""
+  sensitive   = true
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   description = ""
+  sensitive   = true
 }
 
