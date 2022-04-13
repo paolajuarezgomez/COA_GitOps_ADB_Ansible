19e9b24a99ec55b66467852b1d475112c3cc6a26
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-04-13 11:15:21 +0200

    Update manual_backup.tf

--- manual_backup.tf
+++ manual_backup.tf
@@ -10,11 +10,6 @@ resource "oci_database_autonomous_database_backup" "autonomous_database_backup" 
 
   #Required
   autonomous_database_id = module.adb[0].adb_database.adb_database_id
-  #autonomous_database_id = oci_database_autonomous_database.test_autonomous_database.id
-  #${oci_database_autonomous_database_backup.autonomous_database_backup.autonomous_database_id}
-  #module.adb[0].oci_database_autonomous_database.adb_database.id 
-  #autonomous_database_id = data.oci_database_autonomous_database_backups.test_autonomous_database_backups.autonomous_database_id
-  #autonomous_database_id = "ocid1.autonomousdatabase.oc1.eu-frankfurt-1.antheljrttkvkkiapahu7htnklcqashhxrl6k4cgb3kbrhqd7vkxge75x62a"
   display_name           = "Manual_Backup_${local.current_time}"
   depends_on = [null_resource.configure_backup]
 }
