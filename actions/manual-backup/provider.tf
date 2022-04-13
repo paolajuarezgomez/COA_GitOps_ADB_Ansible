6787f72f6f1715e326508a56b7ee888c9e559329
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-04-13 11:15:38 +0200

    Delete actions/manual-backup directory

--- actions/manual-backup/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_id
-  user_ocid        = var.user_id
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
-}
