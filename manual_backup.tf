517c0f06cfd626dfa71a9b14309fae90259610d4
paolajuarezgomez <paola.juarez.gomez@gmail.com>
2022-04-13 09:58:17 +0200

    database id

--- manual_backup.tf
+++ manual_backup.tf
@@ -9,7 +9,8 @@ resource "oci_database_autonomous_database_backup" "autonomous_database_backup" 
   # Create the backup only if the autonomous database id is provided and has been configured for backups.
 
   #Required
-  autonomous_database_id = module.adb[*].adb_database.adb_database_id
+  #autonomous_database_id = module.adb[*].adb_database.adb_database_id
+  autonomous_database_id = data.oci_database_autonomous_database_backups.test_autonomous_database_backups.autonomous_database_id
   display_name           = "Manual_Backup_${local.current_time}"
 
   depends_on = [null_resource.configure_backup]
