2090ee00873468e6724bebf851cdaab45625fa52
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-05-10 11:24:22 +0200

    Delete scripts directory

--- scripts/connect_atp.sh
+++
@@ -1,6 +0,0 @@
-
-export LD_LIBRARY_PATH=/usr/lib/oracle/19.10/client64/lib
-
-sudo unzip -o /tmp/adb_wallet.zip -d /usr/lib/oracle/19.10/client64/lib/network/admin/
-
-sqlplus ADMIN/${password}@${ATP_alias} "@/tmp/config_backup.sql"
