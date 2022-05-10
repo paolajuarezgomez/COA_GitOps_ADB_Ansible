e0e3177bc920d8a3b452774e7609eacef90fe3f8
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-05-10 11:15:35 +0200

    Create config_Backup.sql

---
+++ ansible/files/confgBackup/config_Backup.sql
@@ -0,0 +1,24 @@
+SET HEAD OFF
+SET AUTOPRINT OFF
+SET TERMOUT OFF
+SET SERVEROUTPUT ON
+SPOOL salida.log
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+ALTER DATABASE PROPERTY SET default_backup_bucket='https://swiftobjectstorage.region.oraclecloud.com/v1/namespace/atp_bucket';
+SET DEFINE OFF
+BEGIN
+  DBMS_CLOUD.CREATE_CREDENTIAL(
+    credential_name => 'ATP',
+    username => 'cred_username',
+    password => 'cred_password'
+);
+END;
+/
+ALTER DATABASE PROPERTY SET default_credential = 'ADMIN.ATP';
+BEGIN
+   DBMS_CLOUD.DROP_CREDENTIAL('ADMIN.ATP');
+END;
+/
+select owner, credential_name, username, enabled from dba_credentials;
+select object_name, bytes from dbms_cloud.list_objects('ATP','https://swiftobjectstorage.region.oraclecloud.com/v1/namespace/atp_bucket');
+exit;
